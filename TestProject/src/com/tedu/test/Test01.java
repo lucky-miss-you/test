5925a7bd57462f37c9e2a56c5ff907fd5efeee44
UID <UID@uid-PC>
2019-11-22 13:12:47 +0800

    253

--- TestProject/src/com/tedu/test/Test01.java
+++ TestProject/src/com/tedu/test/Test01.java
@@ -3,7 +3,7 @@ package com.tedu.test;
 public class Test01 {
 
 	public static void main(String[] args) {
-		System.out.println("21");
+		System.out.println("212");
 		// TODO Auto-generated method stub
 
 	}
